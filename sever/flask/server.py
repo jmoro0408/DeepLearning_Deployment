91e838bcd4db59d9696e6a91cfa44e942c10f845
James Moro <james.moro.11@gmail.com>
2021-06-05 16:47:51 -0700

    refactored for AWS hosting

--- sever/flask/server.py
+++ sever/flask/server.py
@@ -1,21 +1,17 @@
-"""
-server
-client -> POST request -> Server ->prediction back to client
-"""
-from flask import Flask, request, jsonify
 import random
-from keyword_spotting_service import Keyword_Spotting_Service
 import os
-
+from flask import Flask, request, jsonify
+from keyword_spotting_service import Keyword_Spotting_Service
 
 app = Flask(__name__)
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
+
     # get audio file and save it
     audio_file = request.files["file"]
-    file_name = str(random.randint(0, 100_000))
+    file_name = str(random.randint(0, 100000))
     audio_file.save(file_name)
 
     # invoke keyword spotting service
@@ -23,9 +19,11 @@ def predict():
 
     # make a prediction
     predicted_keyword = kss.predict(file_name)
-    # remove audio file
+
+    # remove the audio file
     os.remove(file_name)
-    # send back the predited keywork in json format
+
+    # send back the predicted keyword in json format
     data = {"keyword": predicted_keyword}
     return jsonify(data)
 
